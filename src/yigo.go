2bc4c52aa379488098afc5b7047e5113ab67416c
bryanmontalvan <bmontalv@redhat.com>
2022-02-26 14:32:53 -0500

    Add and Get now support zipcodes For better accurary you can now input your cities zipcode
    Changes:
    - Renamed HandleSavedGet() function
    - Created printWeather for reusability
    - Removed conditionals on `HandleSaved()` and `HandleGet()` for allow
      zipcodes
    - Moved checkTemp to `yigo.go`

--- src/yigo.go
+++ src/yigo.go
@@ -57,9 +57,40 @@ func getWeather(cityI string) (Weathers Weather) {
 	return responseObject
 }
 
+func printWeather(Weathers Weather, sigo string, note string) {
+	fmt.Printf("\nCITY: %v, %v\nTEMP: %vf / %vc \nSIGO: %v \n", Weathers.Location.Name, Weathers.Location.Country, Weathers.Current.TempF, Weathers.Current.TempC, sigo)
+	fmt.Printf("\nNote: %v \n\n", note)
+}
+
 func WriteToFile(addCmd *flag.FlagSet, cityweather *string) {
 	err := ioutil.WriteFile("storedcity.txt", []byte(*cityweather), 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+// Takes in location Object, returns string
+func checkTemp(temp int) (sigo string, noteO string) {
+	if temp < 10 {
+		return "NO", "Stay in your house if you want to survive"
+	} else if temp < 20 {
+		return "Noooo", "You're going to freeze to death!"
+	} else if temp < 30 {
+		return "No", "You should put on a scarf"
+	} else if temp < 40 {
+		return "Maybe", "It's cold, but if you want to then go for it"
+	} else if temp < 50 {
+		return "Yes", "Make sure to wear some long clothing"
+	} else if temp < 65 {
+		return "Yes", "Enjoy the chill weather ;)"
+	} else if temp <= 80 {
+		return "Yes", "It's nice out, go out there :)"
+	} else if temp <= 90 {
+		return "Yes", "Beach weather, go get that tan!"
+	} else if temp <= 96 {
+		return "No", "It's very hot, up to you if you would like to get sweaty"
+	} else if temp <= 105 {
+		return "NOO", "Our planet will try up soon, do not go outside!"
+	}
+	return "maybe", "eh"
 }
